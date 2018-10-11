66b3f1d030694ab7262c96160d12020e63b5a8a0
Gleb Galkin <artdaw@gmail.com>
2018-10-11 17:52:44 +0200

    add small example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
+import { curry } from 'ramda';
+
+const split = curry((delimiter, str) => str.split(delimiter));
+const words = split(' ');
+
+document.getElementById('app').innerHTML = words('Test string');
